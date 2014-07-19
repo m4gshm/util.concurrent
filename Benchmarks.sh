fe304ba6a8dfa066007cfe68e8fc4210baaec4f2
alexander <mfourgeneralsherman@gmail.com>
2014-07-19 15:52:02 +0400

    bench fix

--- Benchmarks.sh
+++ Benchmarks.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -ea -Xmx1g -jar ./build/distributions/util.concurrent-1.0-shadow.jar ".*Benchmark1.*" -wi 5 -i 10 -f 0
-
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -ea -Xmx1g -jar ./build/distributions/util.concurrent-0.3-B-shadow.jar ".*Benchmark1.*" -wi 5 -i 10 -f 0
 
